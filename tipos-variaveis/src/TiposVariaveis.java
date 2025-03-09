3f4f6b40f903dc749cd2654f2aaf34c00e3e2685
Beatriz <biawandekoken905@gmail.com>
2025-03-09 12:14:09 -0300

    criando o projeto com os exemplos de tipos variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,29 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       //tipos primitivos
+       // estudar a classe String que representa texto na aplicação
+
+       String meuNome = "Beatriz Teles";
+       String apelido = "Bea";
+
+       if (meuNome.equals(apelido)) {
+        System.out.println("O nome e o apelido são iguais!");
+       } else {
+        System.out.println("O nome e o apelido são diferentes!");
+       }
+       
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+         int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
